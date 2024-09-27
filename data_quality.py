41eb708b01de473d64405e5c6125a90f961f3549
amandalmeida2 <seuemail@example.com>
2024-09-27 19:28:13 -0300

    Adicao de data_sete, notebook, e plot de categoricas

--- data_quality.py
+++ data_quality.py
@@ -9,31 +9,43 @@ class Data_quality:
         self.df = df
 
     def nulos(self):
-        print("contagem de nulos")
-        print(self.df.isnull().sum)
-        print("\n")
+        print(f"--------------------------- \nContagem de nulos\n")
+        print(self.df.isnull().sum().reset_index())
+        print(f"--------------------------- \n")
     
     def unicos(self):
-        print("Contagem de únicos")
-        print(self.df.unique())
-        print("\n")
+        print(f"--------------------------- \nContagem de únicos\n")
+        print(self.df.nunique())
+        print(f"--------------------------- \n")
 
     def contagem_categorica(self):
-        print("Contagem de valores para colunas categoricas")
-        colunas_categorias = self.df.select_dtype(include=['object', 'category']).columns
+        print(f"--------------------------- \nContagem de valores para colunas categoricas")
+        colunas_categorias = self.df.select_dtypes(include=['object', 'category']).columns        
         
         for coluna in colunas_categorias:
             print(f'\nColuna:{coluna}')
             print(self.df[coluna].value_counts())
+            print(f"\n")
 
     def contagem_numericas(self):
-        print("Contagem de valores para colunas numéricas")
+        print(f"--------------------------- \nContagem de valores para colunas numéricas")
         print(self.df.describe())
+        print(f"--------------------------- \n")
+
+    def plotar_categoricas(self):
+        colunas_categorias = self.df.select_dtypes(include=['object', 'category']).columns
+        for coluna in colunas_categorias:
+            plt.figure(figsize=(4,2))
+            sn.countplot(x=coluna, data=self.df)
+            plt.title(f'Coluna categorica: {coluna}')
+            plt.xticks(rotation=45)
+            plt.show()
 
     def gerar_relatorio(self):
         self.nulos()
         self.unicos()
         self.contagem_categorica()
         self.contagem_numericas()
+        self.plotar_categoricas()
         
 
